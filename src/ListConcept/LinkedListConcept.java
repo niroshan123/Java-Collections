754eb4c0c38efefc51da695c7406db5c6ff6428b
niroshan123 <Niroshan07@bitbucket.com>
2020-04-19 17:08:17 +0530

    LinkedListConcept Completed

--- src/ListConcept/LinkedListConcept.java
+++ src/ListConcept/LinkedListConcept.java
@@ -1,5 +1,6 @@
 package ListConcept;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 
 public class LinkedListConcept {
@@ -35,9 +36,41 @@ public class LinkedListConcept {
 
         //remove an element
         ll.removeFirst();
-        ll.remove(0);
+        ll.removeLast();
+        ll.remove(2);
 
         System.out.println("Content of LinkedList "+ll);
+
+        //how to print all the values of linkedlist how to iterate
+        //forloop
+        System.out.println("***Using Simple For loop***");
+        for(int i=0;i<ll.size();i++){
+            System.out.println(ll.get(i));
+        }
+        System.out.println("***Using Simple For Each loop***");
+        //advanced for loop
+        for(String str:ll){
+            System.out.println(str);
+        }
+
+
+        //iterator
+        System.out.println("***Using Iterartor***");
+
+        Iterator<String> it1= ll.iterator();
+        while(it1.hasNext()){
+            String x=it1.next();
+            System.out.println(x);
+
+        }
+        ////while loop
+        System.out.println("***Using While Loop***");
+        int num=0;
+        while(ll.size()>num){
+            System.out.println(ll.get(num));
+            num++;
+        }
+
 
     }
 
